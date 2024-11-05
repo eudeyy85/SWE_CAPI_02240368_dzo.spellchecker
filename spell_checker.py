9891aa7c36a395aa39f07d483415a781d94f553d
eudeyy85 <teudey.10@gmail.com>
2024-11-05 17:56:30 +0600

    practical

--- spell_checker.py
+++ spell_checker.py
@@ -1,10 +1,10 @@
 import re
-def extract_words(text):
+def checker(text):
     words = re.findall(r'[\u0F00-\u0FFF]+', text)
     unique_word = {word for word in words}  
     return unique_word
-words = extract_words(open("368.txt", encoding="utf-8").read())
-words1 = extract_words(open("cleaned_dictionary.txt", encoding="utf-8").read())
+words = checker(open("368.txt", encoding="utf-8").read())
+words1 = checker(open("cleaned_dictionary.txt", encoding="utf-8").read())
 unique_file = words - words1
 for word in unique_file:
     print(f"The word '{word}' from this sentence is incorrect.")
